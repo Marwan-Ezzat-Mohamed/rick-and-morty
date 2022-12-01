d41dfbb70910316c8bb57d4c34c6a9f544223941
marwan-ezzat-mohamed <marwan.ezzat.badawi@gmail.com>
2022-12-01 17:45:21 +0200

    update stores and test

--- src/components/Characters/CharacterGrid/CharacterGrid.stories.tsx
+++ src/components/Characters/CharacterGrid/CharacterGrid.stories.tsx
@@ -25,22 +25,58 @@ Primary.args = {
   gridItems: mockCharacters.characters.results.map(character => ({
     character,
   })),
-  isItemLoaded: (index: number) => true,
-  loadMoreItems: (startIndex: number, stopIndex: number) => {},
+  isItemLoaded: ({ index }: { index: number }) => true,
+  loadMoreItems: ({
+    startIndex,
+    stopIndex,
+  }: {
+    startIndex: number;
+    stopIndex: number;
+  }) => {
+    return new Promise<void>(resolve => {
+      setTimeout(() => {
+        resolve();
+      }, 1000);
+    });
+  },
   hasMore: true,
 };
 
 export const NoResults = Template.bind({});
 NoResults.args = {
   gridItems: [],
-  isItemLoaded: (index: number) => true,
-  loadMoreItems: (startIndex: number, stopIndex: number) => {},
+  isItemLoaded: ({ index }: { index: number }) => true,
+  loadMoreItems: ({
+    startIndex,
+    stopIndex,
+  }: {
+    startIndex: number;
+    stopIndex: number;
+  }) => {
+    return new Promise<void>(resolve => {
+      setTimeout(() => {
+        resolve();
+      }, 1000);
+    });
+  },
   hasMore: false,
 };
 export const Loading = Template.bind({});
 Loading.args = {
   gridItems: null,
-  isItemLoaded: (index: number) => true,
-  loadMoreItems: (startIndex: number, stopIndex: number) => {},
+  isItemLoaded: ({ index }: { index: number }) => true,
+  loadMoreItems: ({
+    startIndex,
+    stopIndex,
+  }: {
+    startIndex: number;
+    stopIndex: number;
+  }) => {
+    return new Promise<void>(resolve => {
+      setTimeout(() => {
+        resolve();
+      }, 1000);
+    });
+  },
   hasMore: false,
 };
--- src/components/Characters/CharacterGrid/CharactersGrid.test.tsx
+++ src/components/Characters/CharacterGrid/CharactersGrid.test.tsx
@@ -2,22 +2,62 @@ import '@testing-library/jest-dom';
 import { render } from '@testing-library/react';
 import CharacterGrid from './CharactersGrid';
 import { mockCharacters } from './../../../__mocks__/characters.mock';
+import { Box, CssBaseline } from '@mui/material';
 
 const characters = mockCharacters.characters.results.map(character => ({
   character,
 }));
+
+const renderComponent = (
+  characters: any,
+  isItemLoaded: any,
+  loadMoreItems: any,
+) => (
+  <div
+    style={{
+      height: '100vh',
+      display: 'flex',
+      flexDirection: 'column',
+    }}
+  >
+    <CharacterGrid
+      gridItems={characters}
+      isItemLoaded={isItemLoaded}
+      loadMoreItems={loadMoreItems}
+      hasMore={false}
+    />
+  </div>
+);
+
+Object.defineProperties(window.HTMLElement.prototype, {
+  offsetLeft: {
+    get: function () {
+      return parseFloat(window.getComputedStyle(this).marginLeft) || 0;
+    },
+  },
+  offsetTop: {
+    get: function () {
+      return parseFloat(window.getComputedStyle(this).marginTop) || 0;
+    },
+  },
+  offsetHeight: {
+    get: function () {
+      return parseFloat(window.getComputedStyle(this).height) || 1080;
+    },
+  },
+  offsetWidth: {
+    get: function () {
+      return parseFloat(window.getComputedStyle(this).width) || 1920;
+    },
+  },
+});
 
 describe('CharacterGrid', () => {
   it('matches snapshot with data', () => {
     const isItemLoaded = jest.fn();
     const loadMoreItems = jest.fn();
     const rendered = render(
-      <CharacterGrid
-        gridItems={characters}
-        isItemLoaded={isItemLoaded}
-        loadMoreItems={loadMoreItems}
-        hasMore={true}
-      />,
+      renderComponent(characters, isItemLoaded, loadMoreItems),
     );
 
     expect(rendered).toMatchSnapshot();
@@ -27,12 +67,7 @@ describe('CharacterGrid', () => {
     const isItemLoaded = jest.fn();
     const loadMoreItems = jest.fn();
     const rendered = render(
-      <CharacterGrid
-        gridItems={[]}
-        isItemLoaded={isItemLoaded}
-        loadMoreItems={loadMoreItems}
-        hasMore={true}
-      />,
+      renderComponent(characters, isItemLoaded, loadMoreItems),
     );
 
     expect(rendered).toMatchSnapshot();
@@ -42,12 +77,7 @@ describe('CharacterGrid', () => {
     const isItemLoaded = jest.fn();
     const loadMoreItems = jest.fn();
     const rendered = render(
-      <CharacterGrid
-        gridItems={null}
-        isItemLoaded={isItemLoaded}
-        loadMoreItems={loadMoreItems}
-        hasMore={true}
-      />,
+      renderComponent(characters, isItemLoaded, loadMoreItems),
     );
 
     expect(rendered).toMatchSnapshot();
@@ -57,12 +87,7 @@ describe('CharacterGrid', () => {
     const isItemLoaded = jest.fn();
     const loadMoreItems = jest.fn();
     const rendered = render(
-      <CharacterGrid
-        gridItems={characters}
-        isItemLoaded={isItemLoaded}
-        loadMoreItems={loadMoreItems}
-        hasMore={true}
-      />,
+      renderComponent(characters, isItemLoaded, loadMoreItems),
     );
 
     const grid = rendered.container.querySelector('.virtual-grid>div');
@@ -75,12 +100,7 @@ describe('CharacterGrid', () => {
     const isItemLoaded = jest.fn();
     const loadMoreItems = jest.fn();
     const rendered = render(
-      <CharacterGrid
-        gridItems={characters}
-        isItemLoaded={isItemLoaded}
-        loadMoreItems={loadMoreItems}
-        hasMore={true}
-      />,
+      renderComponent(characters, isItemLoaded, loadMoreItems),
     );
 
     const grid = rendered.container.querySelector('.virtual-grid>div');
