839c3f0ee288b464b74675a180c88159ef5c4c71
marwan-ezzat-mohamed <marwan.ezzat.badawi@gmail.com>
2022-12-02 16:35:48 +0200

    removed unused code

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { ThemeProvider, CssBaseline, Box } from '@mui/material';
 import NavBar from './components/NavBar';
 import { ColorModeContext, useMode } from './Theme/theme';
 import Characters from './components/Characters';
-import MyCssBaseLine from './MyCssBaseLine';
 
 const App = (): JSX.Element => {
   const [theme, toggleTheme] = useMode();
@@ -10,7 +9,7 @@ const App = (): JSX.Element => {
     <ColorModeContext.Provider value={toggleTheme}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <MyCssBaseLine />
+
         <Box
           sx={{
             height: '100vh',
